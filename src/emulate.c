2904200d536bf4e2159a86dd88f67df43c1b7c95
1stub <stubbiegaming@gmail.com>
2025-04-15 07:37:28 -0400

    readme

--- src/emulate.c
+++ src/emulate.c
@@ -3,9 +3,19 @@
 
 #include "../include/emulate.h"
 
+//
+// Something kinda odd is even though the timing feels right
+// it is very clear we are rendering at ~30fps rather than 60.
+// Will need to give this a better look
+//
+
 void emulate() {
     int quit = false;
-    const Uint32 frame_delay = 32; // Force 60fps
+    //
+    // This number should be 16 ms but it appears we are
+    // runnign 4x speed at 16ms per frame
+    //
+    const Uint32 frame_delay = 64; // Force 60fps
     Uint32 frame_start;
     Uint32 frame_time;
     
